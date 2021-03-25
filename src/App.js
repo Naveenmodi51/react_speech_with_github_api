fa4ee5f71c3367b3d74725daf346aa77c333ef91
naveen <naveen@gamil.com>
2021-03-25 20:12:53 +0530

    github_project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from "react";
+import SearchComponent from "./Component/SearchComponent";
+import axios from "axios";
+import DisplayGitUsers from "./Component/DisplayGitUsers";
+class App extends Component {
+  state = {
+    term: "",
+    reposData: "",
+    loading: false,
+  };
+  onTermSubmit = async term => {
+    let client_id="Iv1.0ea4403847829ece";
+    let client_secret="1378c41a6bae500e6e2d8adcb3dbc4290d1ddc48";
+    let response = await axios.get(
+      `https:api.github.com/users/${term}?Client_Id${client_id}&Client_Secret${client_secret}`
+    );
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        let repos = await axios.get(
+      `https://api.github.com/users/${term}/repos?Client_Id${client_id}&Client_Secret${client_secret}`
+    );
+
+    this.setState({ term: response.data, reposData: repos, loading: true });
+  };
+  render() {
+    return (
+      <Fragment>
+        <SearchComponent onTermSubmit={this.onTermSubmit} />
+        <section className="container my-2">
+          <hr className="hr" />
+          <DisplayGitUsers
+            users={this.state.term}
+            repos={this.state.reposData}
+            loading={this.state.loading}
+          />
+        </section>
+      </Fragment>
+    );
+  }
 }
 
 export default App;
